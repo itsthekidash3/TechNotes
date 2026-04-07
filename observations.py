d40aed86911d209167e81d91ebea77d7241ed6d4
Akshanth Mamidala <119917995+itsthekidash3@users.noreply.github.com>
2026-04-06 18:47:15 -0600

    comments on uses

--- observations.py
+++ observations.py
@@ -1,1 +1,5 @@
+# java is used for large scale enterprise coz of scalbility
 
+# and go is also preffered for backend
+
+# python is used for automation . I like python simple, easy reliable and msotly automated.
