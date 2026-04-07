c25ef81a9cd628d1cb0c88f6e95fcc78af2ae494
Akshanth Mamidala <119917995+itsthekidash3@users.noreply.github.com>
2026-04-06 19:09:47 -0600

    Update observations.py

--- observations.py
+++ observations.py
@@ -1,5 +1,9 @@
-# java is used for large scale enterprise coz of scalbility
+# java is used for large scale enterprise coz of scalbility and JVM s.
 
-# and go is also preffered for backend
+# and go is also preffered for backend. Has go routines, used for scalability. more like a stick car, map .=, 
 
-# python is used for automation . I like python simple, easy reliable and msotly automated.
+# python is used for automation . I like python simple, easy reliable and msotly automated. Used for ease. Patient friend AI
+
+# The best language is the one you’ll actually use to build things
+
+# js is ease . coz used for both front end and back end. promise. then, catch
