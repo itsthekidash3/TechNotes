8ab651a18e2304129b773f042201aec3196f72db
Akshanth Mamidala <119917995+itsthekidash3@users.noreply.github.com>
2026-04-25 11:46:50 -0600

    Add comments about JSON schema in observations.js
    Added comments explaining JSON schema and its structure.

--- observations.js
+++ observations.js
@@ -1,1 +1,16 @@
 // async, await, promise, then, catch -> call back hell !!!!
+
+
+// JSON is that format. It's the universal language of APIs
+
+// A JSON schema describes the shape of data
+/* {                                                         
+      "type": "object",                                                                                                                                                           
+      "properties": {                                       
+          "field_name": {
+              "type": "string or number or boolean or array or object",
+              "description": "what this field is"                                                                                                                                 
+          }
+      },                                                                                                                                                                          
+      "required": ["field_name"]                            
+  } */
